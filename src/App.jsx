fe5e044392b716cf49af79deac6af81b7e040a6b
hassaanjamshed <wearehassaan@gmail.com>
2024-08-24 19:02:02 +0500

    vite+project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h3 className='text-center bg-[#64748b] text-3xl'>welcome to tailwind css</h3>
+    </div>
+  )
+}
+
+export default App
